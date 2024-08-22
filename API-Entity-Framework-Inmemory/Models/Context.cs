ab93e42a677d58d349b77511227ab31b20f9825a
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-08-22 16:05:40 -0300

    Atualização do pacote Swashbuckle.AspNetCore; Adição do pacote Microsoft.EntityFrameworkCore.InMemory; Configuração do contexto para alocar os dados InMemory

---
+++ API-Entity-Framework-Inmemory/Models/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace API_Entity_Framework_Inmemory.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+        public DbSet<Context> DbSet { get; set; } = null;
+    }
+}
