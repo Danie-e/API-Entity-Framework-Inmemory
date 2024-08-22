cc1b0018f4085cbb4a7f4d7778bf02872122f79b
Daniela Eloisa <danielaeloisa1@gmail.com>
2024-08-22 15:44:57 -0300

    Adição da pasta Models com a classe Usuario

---
+++ API-Entity-Framework-Inmemory/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace API_Entity_Framework_Inmemory.Models
+{
+    public class Usuario
+    {
+        public string nome { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+        public int codigoDePessoa { get; set; }
+        public string lembreteDeSenha { get; set; }
+        public int idade { get; set; }
+        public string sexo { get; set; }
+    }
+}
